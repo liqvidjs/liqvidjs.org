f49af5b029bd14cf4106f4d10210263ef08ea69d
Yuri Sulyma <yuri@lmqm.xyz>
2020-05-24 22:49:43 -0400

    Force HTTPS

--- server.ts
+++ server.ts
@@ -3,10 +3,10 @@ import * as express from "express";
 const app = express();
 if (app.get("env") === "production" && process.env.HEROKU) {
   app.use((req, res, next) => {
-    // if (req.header("x-forwarded-proto") !== "https")
-      // res.redirect(`https://${req.header("host")}${req.url}`);
-    // else
-      // next();
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else
+      next();
   });
 }
 app.disable("x-powered-by");
