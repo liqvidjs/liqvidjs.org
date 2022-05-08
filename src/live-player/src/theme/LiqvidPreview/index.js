e058b85ee070946fec9ceab3b01ebacd0eb8b5aa
Yuri Sulyma <>
2022-05-08 04:31:46 -0400

    Improve version pinning in live-player

--- src/live-player/src/theme/LiqvidPreview/index.js
+++ src/live-player/src/theme/LiqvidPreview/index.js
@@ -22,7 +22,24 @@ import {HTMLEditor} from "./HTMLEditor";
 import {TSXEditor} from "./TSXEditor";
 
 const Mod = (!ExecutionEnvironment.canUseDOM || navigator.platform === "MacIntel" ? "Cmd" : "Ctrl");
-const liqvidVersion = "2.1.4";
+const VERSIONS = {
+  "liqvid": "2.1.6",
+  "@liqvid/mathjax": "0.0.2",
+  "@react-three/fiber": "8.0.13",
+  "react": "17.0.2",
+  "react-dom": "17.0.2",
+  "three": "0.140.0"
+}
+
+const DEPS = {
+  "liqvid": ["react", "react-dom"],
+  "@liqvid/katex": ["liqvid", "react"],
+  "@liqvid/mathjax": ["liqvid", "react"],
+  "@liqvid/xyjax": ["@liqvid/mathjax", "liqvid", "react"],
+  "@liqvid/react-three": ["liqvid", "@react-three/fiber", "react", "three"],
+  "@react-three/fiber": ["react", "three"],
+  "react-dom": ["react"]
+};
 
 export default function Playground({children, transformCode, ...props}) {
   const tsxView = useRef();
@@ -129,7 +146,7 @@ export default function Playground({children, transformCode, ...props}) {
   );
 }
 
-function render({tsx, css="", module, head = ""}) {
+function render({tsx, css = "", module, head = ""}) {
   const opts = {
     filename: "demo.tsx",
     presets: ["react", "typescript"]
@@ -161,7 +178,7 @@ function render({tsx, css="", module, head = ""}) {
    <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1"/>
  
    <!-- Liqvid -->
-   <link href="https://unpkg.com/liqvid@${liqvidVersion}/dist/liqvid.min.css" rel="stylesheet" type="text/css"/>
+   <link href="https://unpkg.com/liqvid@${VERSIONS.liqvid}/dist/liqvid.min.css" rel="stylesheet" type="text/css"/>
    <style id="user-styles" type="text/css">${css}</style>
    <script type="text/javascript">
    window.addEventListener("message", msg => {
@@ -178,13 +195,48 @@ function render({tsx, css="", module, head = ""}) {
    `;
 
     if (module) {
+      doc += String.raw`<!--
+      JSPM Generator Import Map
+      Edit URL: https://generator.jspm.io/#U2VhYGCVD80rySzJSU1hcChKTUwu0S3JKEpN1U/LTEotcrDQM9AzNGbIySwsy0xxMNIz1DNjgKhKyc91MDQHShtBBGAcsG4HoC4TAz0DAAIAz8ZhAA
+    -->
+    <script type="importmap">
+    {
+      "imports": {
+        "@react-three/fiber": "https://ga.jspm.io/npm:@react-three/fiber@8.0.13/dist/react-three-fiber.esm.js",
+        "liqvid": "https://ga.jspm.io/npm:liqvid@2.1.6/dist/liqvid.mjs",
+        "react": "https://ga.jspm.io/npm:react@17.0.2/dev.index.js",
+        "react-dom": "https://ga.jspm.io/npm:react-dom@17.0.2/dev.index.js",
+        "three": "https://ga.jspm.io/npm:three@0.140.0/build/three.module.js"
+      },
+      "scopes": {
+        "https://ga.jspm.io/": {
+          "@babel/runtime/helpers/esm/extends": "https://ga.jspm.io/npm:@babel/runtime@7.17.9/helpers/esm/extends.js",
+          "debounce": "https://ga.jspm.io/npm:debounce@1.2.1/index.js",
+          "object-assign": "https://ga.jspm.io/npm:object-assign@4.1.1/index.js",
+          "process": "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.24/nodelibs/browser/process.js",
+          "react": "https://ga.jspm.io/npm:react@18.1.0/dev.index.js",
+          "react-merge-refs": "https://ga.jspm.io/npm:react-merge-refs@1.1.0/dist/react-merge-refs.esm.js",
+          "react-reconciler": "https://ga.jspm.io/npm:react-reconciler@0.27.0/dev.index.js",
+          "react-reconciler/constants": "https://ga.jspm.io/npm:react-reconciler@0.27.0/dev.constants.js",
+          "react-use-measure": "https://ga.jspm.io/npm:react-use-measure@2.1.1/dist/web.js",
+          "scheduler": "https://ga.jspm.io/npm:scheduler@0.21.0/dev.index.js",
+          "scheduler/tracing": "https://ga.jspm.io/npm:scheduler@0.20.2/dev.tracing.js",
+          "suspend-react": "https://ga.jspm.io/npm:suspend-react@0.0.8/dist/index.js",
+          "zustand": "https://ga.jspm.io/npm:zustand@3.7.2/esm/index.js"
+        },
+        "https://ga.jspm.io/npm:react-dom@17.0.2/": {
+          "scheduler": "https://ga.jspm.io/npm:scheduler@0.20.2/dev.index.js"
+        }
+      }
+    }
+    </script>`;
       doc += String.raw`<script type="module">${esmShImports(js)}</script>`;
     } else {
       doc += String.raw`<!-- production -->
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/17.0.1/umd/react.production.min.js"></script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.1/umd/react-dom.production.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/17.0.2/umd/react.production.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.2/umd/react-dom.production.min.js"></script>
      
-     <script src="https://unpkg.com/liqvid@${liqvidVersion}/dist/liqvid.min.js"></script>
+     <script src="https://unpkg.com/liqvid@${VERSIONS.liqvid}/dist/liqvid.min.js"></script>
      <script crossorigin integrity="sha384-ImWMbbJ1rSn1mn+2vsKm/wN6Vc7hPNB2VKN0lX3FAzGK+c7M2mD6ZZcwknuKlP7K" src="https://cdn.rangetouch.com/2.0.1/rangetouch.js"></script>
    
      <script>${js}</script>`;
@@ -200,11 +252,21 @@ function render({tsx, css="", module, head = ""}) {
 }
 
 function esmShImports(str) {
-  return str.replace(/^(import .+? from\s+)(["'])(.+?)\2(;?)$/gm, (match, start, q, name, end) => {
+  return str.replace(/(import .+? from\s+)(["'])(.+?)\2(;?)/gm, (match, start, q, name, end) => {
+    return match;
+    const pkgName = name;
+
     // https://github.com/esm-dev/esm.sh/issues/276
-    if (name.startsWith("@liqvid")) {
-      name += `?deps=liqvid@${liqvidVersion}`;
+    if (pkgName in VERSIONS) {
+      name += `@${VERSIONS[pkgName]}`;
+    }
+    if (pkgName in DEPS) {
+      name += `?deps=` + deps(...DEPS[pkgName]);
     }
     return `${start}${q}https://esm.sh/${name}${q}${end}`;
   });
+}
+
+function deps(...keys) {
+  return keys.map(key => `${key}@${VERSIONS[key]}`).join(",");
 }
